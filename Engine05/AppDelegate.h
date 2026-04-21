321b78b902dcd22cf52b7eac78e50238f5875a08
Donggon-sys <63091200+Donggon-sys@users.noreply.github.com>
2026-04-21 19:02:34 +0800

    Initial Commit

---
+++ Engine05/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Engine05
+//
+//  Created by Chenruyi on 2026/4/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Engine05/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Engine05
+//
+//  Created by Chenruyi on 2026/4/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
